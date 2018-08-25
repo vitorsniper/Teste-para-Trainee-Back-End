3f905c589ad786f860ff6477593a6a7a87a99a5a
vitorsniper <bai_ferreira@hotmail.com>
2018-08-25 01:55:28 -0300

    Add files via upload

---
+++ ModeladorDeRetangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Back_End_Program_6
+{
+    class ModeladorDeRetangulo
+    {        
+        public float mudarLadoA(float ladoA)
+        {
+            Console.WriteLine("Digite a nova base:");
+            ladoA = int.Parse(Console.ReadLine());
+            
+            return ladoA;
+        }
+
+        public float mudarLadoB(float ladoB)
+        {
+            Console.WriteLine("Digite a nova altura:");
+            ladoB = int.Parse(Console.ReadLine());
+
+            return ladoB;
+        }
+
+        public float calcularArea(float ladoA, float ladoB)
+        {
+            float area = ladoA * ladoB;
+            return area;
+        }
+
+        public float calcularPerimetro(float ladoA, float ladoB)
+        {
+            float perimetro = 2 * (ladoA + ladoB);
+            return perimetro;
+        }
+
+    }
+
+    
+}
